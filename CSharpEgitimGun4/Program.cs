07df8d327d483e6e9c0e524560d4a6a363eb5ae3
dmrcn294 <dmrcn294@gmail.com>
2024-10-20 20:22:40 +0300

    .

---
+++ CSharpEgitimGun4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimGun4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Dongüsü
+            //for(int i =1; i<=10; i++)
+            //{
+            //    Console.WriteLine("Merhab Dünya");
+            //}
+            //--------------------------------------------------
+            //Console.Write("Yazdırmak İstediğiniz Metni Girin : ");
+            //string ifade;
+            //ifade= Console.ReadLine();
+            //Console.WriteLine();
+            //Console.Write("Yazdırmak İstediğiniz Tekrar Sayısı  : ");
+            //int repeat;
+            //repeat = int.Parse(Console.ReadLine());
+            //for(int i =1;i<=repeat;i++)
+            //{
+            //    Console.WriteLine(ifade);
+            //}
+            //--------------------------------------------------------
+
+
+
+            #endregion
+
+
+            #region While Döngüsü
+
+            //int a = 1;
+            //while (a<10)
+            //{
+            //    Console.WriteLine("Merhaba Dünya");
+            //    a++;
+
+
+            //}
+            //-----------------------------------------
+            //Console.Write("İsim Giriniz : ");
+            //string ad = Console.ReadLine();
+            //Console.Write("Tekrar : ");
+            //int a = int.Parse(Console.ReadLine());
+            //int i = 0;
+            //while(i<a)
+            //{
+            //    Console.WriteLine(ad);
+            //    i++;
+
+            //}
+
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpEgitimGun5/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimGun5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 10 Tane Yıldız Yazdırma Yan Yana
+            //for(int i=1; i<=10;i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region 10 Tane Yıldız Yazdırma Alt Alta
+            //for(int i=1; i<=10;i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+
+            #region 10 Tane Yıldız 10 Satır
+            //for(int i =1; i<=10; i++)
+            //{
+            //    Console.WriteLine("*********");
+            //}
+
+            #endregion
+
+            #region Dik Üçgen
+            //for(int i=1;i<=5;i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region Ters Dik Üçgen
+            //for(int i =6; i>=1;i--)
+            //{
+            //    for(int j=1;j<i;j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Dilimi
+            //for (int i=1;i<=5;i++)
+            //{
+            //    for (int j = 5; j - i > 0; j--)
+            //    {
+            //        Console.Write(" ");
+
+            //    }
+            //    for(int k=1;k<=2*i-1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 4; i > 0; i--)
+            //{
+            //    for (int j = 5; j - i > 0; j--)
+            //    {
+            //        Console.Write(" ");
+
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region Piramit
+            //for(int i=1;i<=5;i++)
+            //{
+            //    for(int j=5;j-i>=0;j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int k=1;k<=2*i-1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Piramit
+            
+            //for(int i =6;i>=0;i--)
+            //{
+            //    for(int j=7;j-i>1;j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int k=1;k<=2*i-1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
